461fa8605bec38de4da4f8c0f500e284aa5fd47c
Alexandra Meshi <109416988+svechino@users.noreply.github.com>
2025-03-28 09:38:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,6 @@ addEventListener("fetch", event => {
   event.respondWith(handleRequest(event.request))
 })
 
-const CNAME = "neskuchno.net";
-const URL = "https://www.notion.so/Alexandra-Meshi-1a3b759e8bdb80199f57d9d1c69b2957";
-
-async function handleRequest(req) {
-  let url = new URL(req.url)
-  url.hostname = URL.replace(/^https?:\/\//, "").split("/")[0]
-  url.pathname = URL.replace(/^https?:\/\//, "").split("/").slice(1).join("/")
-  return fetch("https://" + url.hostname + "/" + url.pathname)
+async function handleRequest(request) {
+  return Response.redirect("https://www.notion.so/Alexandra-Meshi-1a3b759e8bdb80199f57d9d1c69b2957", 301);
 }
