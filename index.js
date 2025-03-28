b2f2c77c756b70d5fe5ebffcc4d5ea0dc2a84210
Alexandra Meshi <109416988+svechino@users.noreply.github.com>
2025-03-28 09:43:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ addEventListener("fetch", event => {
 async function handleRequest(request) {
   return Response.redirect("https://www.notion.so/Alexandra-Meshi-1a3b759e8bdb80199f57d9d1c69b2957", 301);
 }
+
+// force redeploy
